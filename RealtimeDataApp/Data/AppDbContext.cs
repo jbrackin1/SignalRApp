3712b06ecbf7c642b489f86bd18a5a28a386c10a
Jerod Brackin <jerodbrackin@Jerods-MacBook-Pro.local>
2024-02-06 15:56:19 -0600

    2nd commit

---
+++ RealtimeDataApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealtimeDataApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        string _connectionString = "Server=/Users/jerodbrackin/CompanyDatabase2;Database=CompanyDatabase2;Trusted_Connection=True;";
+
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
